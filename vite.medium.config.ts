caa61bfc98f70ea0e64d9ebb859e184f02e3df9b
toms74209200 <motomotomato@gmail.com>
2025-05-15 01:44:28 +0900

    :art: Format code

--- vite.medium.config.ts
+++ vite.medium.config.ts
@@ -2,10 +2,8 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    test: {
-        include: ["**/*.medium.{test,spec}.?(c|m)[jt]s?(x)"],
-        reporters: Deno.env.get("CI")
-            ? ["verbose", "github-actions"]
-            : ["verbose"],
-    },
+  test: {
+    include: ["**/*.medium.{test,spec}.?(c|m)[jt]s?(x)"],
+    reporters: Deno.env.get("CI") ? ["verbose", "github-actions"] : ["verbose"],
+  },
 });
